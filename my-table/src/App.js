7308e8745317c6503a774f49f5133213b15a5c0f
greyewi1 <greyewi@gmail.com>
2020-10-03 11:29:55 +0300

    useState, useCallback, useEffect

--- my-table/src/App.js
+++ my-table/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initTable = [
+  {"id": 1, "name": "Anya", "description": "description", "info1": "info", "info2": "info", "isChecked" : false},
+  {"id": 2, "name": "Petya", "description": "description", "info1": "info", "info2": "info", "isChecked" : false},
+  {"id": 3, "name": "Stepan", "description": "description", "info1": "info", "info2": "info", "isChecked" : false},
+  {"id": 4, "name": "Vova", "description": "description", "info1": "info", "info2": "info", "isChecked" : false},
+  {"id": 5, "name": "Vasya", "description": "description", "info1": "info", "info2": "info", "isChecked" : false}
+]
+
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const handleChangeCount = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+
+  useEffect(() => {
+    console.log("mount")
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +37,12 @@ function App() {
         >
           Learn React
         </a>
+        <span onClick={handleChangeCount}>
+          {count}
+        </span>
+        {initTable.map((item, key) => {
+          return <div key={key}>{item.name}</div>
+        })}
       </header>
     </div>
   );
