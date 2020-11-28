335eb910a973e1ca4f45e3c4306a0cf31f890183
Maxim <zakharovmm88@gmail.com>
2020-11-28 15:34:51 +0200

    switch

--- my-table/src/components/HandmadeSwitch/index.jsx
+++ my-table/src/components/HandmadeSwitch/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from "prop-types";
 import "./style.css"
 
 const HandmadeSwitch = () => {
@@ -14,10 +13,5 @@ const HandmadeSwitch = () => {
     )
 }
 
-HandmadeSwitch.propTypes = {
-    selected: PropTypes.bool.isRequired,
-    label: PropTypes.array.isRequired,
-    onChange: PropTypes.func.isRequired,
-}
 
 export default HandmadeSwitch;
